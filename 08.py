be165fa67085c32b4c7e1100831d40605536c034
Gabriel Da Silva Pedrosa <162148284+Gabriell190@users.noreply.github.com>
2025-03-29 15:00:37 -0300

    Create 08.py

---
+++ 08.py
@@ -0,0 +1,7 @@
+#Solicite uma temperatura em Celsius e converta para Fahrenheit.
+
+temp = float(input("Digite uma temperatura em Celsius: "))
+
+fahrenheit = (temp * 9/5) + 32
+
+print(f"A temperatura em Fahrenheit é: {fahrenheit:.1f}°F")
